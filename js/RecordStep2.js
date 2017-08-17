34e5353960d44ae71fc0699291e2ed931bd3b772
HwangTaeWook <xowktktjd@gmail.com>
2017-08-18 02:31:26 +0900

    etst

--- js/RecordStep2.js
+++ js/RecordStep2.js
@@ -7,28 +7,3 @@ function onClickRecord() {
     location.replace('../html/RecordStep3.html');
   }
 }
-
-//init_Chart는 html이 모두 로드되었을때 실핵되도록 구성
-function init_Chart(data,label) {
-  var data = {
-    labels: label, //하단 라벨은 요일을 표시
-    datasets: [{
-      label: 'magnitude', //방송사 채널을 표시
-       //시청률을 표시
-      backgroundColor: "rgba(75, 208, 133, 0.3)",
-      data: data
-    }]
-  };
-  var options = {
-      scales: {
-        xAxes: [{
-          display: false
-        }],
-        yAxes: [{
-          display: false
-        }],
-      }
-    };
-  var ctx = document.getElementById('myChart').getContext('2d'); //html의 myChart를 가져와 ctx에 저장
-  var lineChart = new Chart(ctx).Line(data, options);
-}

7e141527ddef0121a81b02c854ebe9d24e98103c
HwangTaeWook <xowktktjd@gmail.com>
2017-08-18 02:40:45 +0900

    change

--- html/js/Chart.js
+++ html/js/Chart.js
@@ -12458,3 +12458,29 @@
 });
 
 //오픈 JS Library를 사용해서 차트를 제작
+//init_Chart는 html이 모두 로드되었을때 실핵되도록 구성
+function init_Chart(data,label) {
+  var ctx = document.getElementById('myChart').getContext('2d'); //html의 myChart를 가져와 ctx에 저장
+  var myChart = new Chart(ctx, { //차트를 양식에 맞춰서 출력
+    type: 'line', //line Type
+    options: {
+        scales: {
+          xAxes: [{
+            display: false
+          }],
+          yAxes: [{
+            display: false
+          }],
+        }
+      },
+    data: {
+      labels: label, //하단 라벨은 요일을 표시
+      datasets: [{
+        label: 'TVN', //방송사 채널을 표시
+         //시청률을 표시
+        backgroundColor: "rgba(75, 208, 133, 0.3)",
+        data: data
+      }]
+    }
+  });
+}
--- html/js/Main.js
+++ html/js/Main.js
@@ -17,9 +17,9 @@ window.AudioContext = window.AudioContext || window.webkitAudioContext;
 
 var audioContext = new AudioContext();
 var audioInput = null,
-  realAudioInput = null,
-  inputPoint = null,
-  audioRecorder = null;
+    realAudioInput = null,
+    inputPoint = null,
+    audioRecorder = null;
 var rafID = null;
 var analyserContext = null;
 var canvasWidth, canvasHeight;
@@ -32,168 +32,179 @@ var recIndex = 0;
 */
 
 function saveAudio() {
-  audioRecorder.exportWAV(doneEncoding);
-  // could get mono instead by saying
-  // audioRecorder.exportMonoWAV( doneEncoding );
+    audioRecorder.exportWAV( doneEncoding );
+    // could get mono instead by saying
+    // audioRecorder.exportMonoWAV( doneEncoding );
 }
 
-function gotBuffers(buffers) {
-  var canvas = document.getElementById("wavedisplay");
+function gotBuffers( buffers ) {
+    var canvas = document.getElementById( "wavedisplay" );
 
-  drawBuffer(canvas.width, canvas.height, canvas.getContext('2d'), buffers[0]);
+    drawBuffer( canvas.width, canvas.height, canvas.getContext('2d'), buffers[0] );
 
-  // the ONLY time gotBuffers is called is right after a new recording is completed -
-  // so here's where we should set up the download.
-  audioRecorder.exportWAV(doneEncoding);
+    // the ONLY time gotBuffers is called is right after a new recording is completed -
+    // so here's where we should set up the download.
+    audioRecorder.exportWAV( doneEncoding );
 }
 
-function doneEncoding(blob) {
-  Recorder.setupDownload(blob, "myRecording" + ((recIndex < 10) ? "0" : "") + recIndex + ".wav");
-  recIndex++;
+function doneEncoding( blob ) {
+    Recorder.setupDownload( blob, "myRecording" + ((recIndex<10)?"0":"") + recIndex + ".wav" );
+    recIndex++;
 }
 
-function toggleRecording(e) {
-  if (e.classList.contains("recording")) {
-    // stop recording
-    audioRecorder.stop();
-    e.classList.remove("recording");
-    audioRecorder.getBuffers(gotBuffers);
-  } else {
-    // start recording
-    if (!audioRecorder)
-      return;
-    e.classList.add("recording");
-    audioRecorder.clear();
-    audioRecorder.record();
-  }
+function toggleRecording( e ) {
+    if (e.classList.contains("recording")) {
+        // stop recording
+        audioRecorder.stop();
+        e.classList.remove("recording");
+        audioRecorder.getBuffers( gotBuffers );
+    } else {
+        // start recording
+        if (!audioRecorder)
+            return;
+        e.classList.add("recording");
+        audioRecorder.clear();
+        audioRecorder.record();
+    }
 }
 
-function convertToMono(input) {
-  var splitter = audioContext.createChannelSplitter(2);
-  var merger = audioContext.createChannelMerger(2);
+function convertToMono( input ) {
+    var splitter = audioContext.createChannelSplitter(2);
+    var merger = audioContext.createChannelMerger(2);
 
-  input.connect(splitter);
-  splitter.connect(merger, 0, 0);
-  splitter.connect(merger, 0, 1);
-  return merger;
+    input.connect( splitter );
+    splitter.connect( merger, 0, 0 );
+    splitter.connect( merger, 0, 1 );
+    return merger;
 }
 
 function cancelAnalyserUpdates() {
-  window.cancelAnimationFrame(rafID);
-  rafID = null;
+    window.cancelAnimationFrame( rafID );
+    rafID = null;
 }
 
 var data3;
 
 function updateAnalysers(time) {
-  if (!analyserContext) {
-    var canvas = document.getElementById("analyser");
-    canvasWidth = canvas.width;
-    canvasHeight = canvas.height;
-    analyserContext = canvas.getContext('2d');
-  }
-
-  // analyzer draw code here
-  {
-    var SPACING = 3;
-    var BAR_WIDTH = 1;
-    var numBars = Math.round(canvasWidth / SPACING);
-    var freqByteData = new Uint8Array(analyserNode.frequencyBinCount);
-
-    analyserNode.getByteFrequencyData(freqByteData);
-
-    analyserContext.clearRect(0, 0, canvasWidth, canvasHeight);
-    analyserContext.fillStyle = '#F6D565';
-    analyserContext.lineCap = 'round';
-    var multiplier = analyserNode.frequencyBinCount / numBars;
-
-    // Draw rectangle for each frequency bin.
-    data2 = new Array(parseInt((numBars - 120) / 10));
-    data3 = parseInt((numBars - 120) / 10);
-    for (var i = 0; i < parseInt((numBars - 120) / 10); ++i) {
-      var magnitude = 0;
-      var offset = Math.floor(i * 5 * multiplier);
-      // gotta sum/average the block, or we miss narrow-bandwidth spikes
-      for (var j = 0; j < multiplier; j++)
-        magnitude += freqByteData[offset + j];
-      magnitude = magnitude / multiplier;
-      var magnitude2 = freqByteData[i * multiplier];
-      // analyserContext.fillStyle = "hsl( " + Math.round((i*360)/numBars) + ", 100%, 50%)";
-      // analyserContext.fillRect(i * SPACING, canvasHeight, BAR_WIDTH, -magnitude);
-
-      data2[i] = magnitude;
-
-      // console.log(i);
-      // console.log(-magnitude);
+    if (!analyserContext) {
+        var canvas = document.getElementById("analyser");
+        canvasWidth = canvas.width;
+        canvasHeight = canvas.height;
+        analyserContext = canvas.getContext('2d');
     }
-  }
 
-  //rafID = window.requestAnimationFrame(updateAnalysers);
+    // analyzer draw code here
+    {
+        var SPACING = 3;
+        var BAR_WIDTH = 1;
+        var numBars = Math.round(canvasWidth / SPACING);
+        var freqByteData = new Uint8Array(analyserNode.frequencyBinCount);
+
+        analyserNode.getByteFrequencyData(freqByteData);
+
+        analyserContext.clearRect(0, 0, canvasWidth, canvasHeight);
+        analyserContext.fillStyle = '#F6D565';
+        analyserContext.lineCap = 'round';
+        var multiplier = analyserNode.frequencyBinCount / numBars;
+
+        // Draw rectangle for each frequency bin.
+        data2 = new Array(parseInt((numBars-120)/10));
+        data3= parseInt((numBars-120)/10);
+        for (var i = 0; i < parseInt((numBars-120)/10); ++i) {
+            var magnitude = 0;
+            var offset = Math.floor( i*5 * multiplier );
+            // gotta sum/average the block, or we miss narrow-bandwidth spikes
+            for (var j = 0; j< multiplier; j++)
+                magnitude += freqByteData[offset + j];
+            magnitude = magnitude / multiplier;
+            var magnitude2 = freqByteData[i * multiplier];
+            // analyserContext.fillStyle = "hsl( " + Math.round((i*360)/numBars) + ", 100%, 50%)";
+            // analyserContext.fillRect(i * SPACING, canvasHeight, BAR_WIDTH, -magnitude);
+
+            data2[i] = magnitude;
+
+            // console.log(i);
+            // console.log(-magnitude);
+        }
+
+
+
+
+    }
+
+    rafID = window.requestAnimationFrame( updateAnalysers );
 }
 var id = setInterval(frame, 100);
-
-function frame() {
-  init_Chart(data2, new Array(data3));
+function frame(){
+  var myNode = document.getElementById("div_chart");
+    while (myNode.firstChild) {
+      myNode.removeChild(myNode.firstChild);
+  }
+  var temp22=document.createElement("canvas");
+temp22.setAttribute("id","myChart");
+myNode.appendChild(temp22);
+  init_Chart(data2,new Array(data3));
 }
 
 function toggleMono() {
-  if (audioInput != realAudioInput) {
-    audioInput.disconnect();
-    realAudioInput.disconnect();
-    audioInput = realAudioInput;
-  } else {
-    realAudioInput.disconnect();
-    audioInput = convertToMono(realAudioInput);
-  }
+    if (audioInput != realAudioInput) {
+        audioInput.disconnect();
+        realAudioInput.disconnect();
+        audioInput = realAudioInput;
+    } else {
+        realAudioInput.disconnect();
+        audioInput = convertToMono( realAudioInput );
+    }
 
-  audioInput.connect(inputPoint);
+    audioInput.connect(inputPoint);
 }
 
 function gotStream(stream) {
-  inputPoint = audioContext.createGain();
+    inputPoint = audioContext.createGain();
 
-  // Create an AudioNode from the stream.
-  realAudioInput = audioContext.createMediaStreamSource(stream);
-  audioInput = realAudioInput;
-  audioInput.connect(inputPoint);
+    // Create an AudioNode from the stream.
+    realAudioInput = audioContext.createMediaStreamSource(stream);
+    audioInput = realAudioInput;
+    audioInput.connect(inputPoint);
 
-  //audioInput = convertToMono( input );
+    //audioInput = convertToMono( input );
 
-  analyserNode = audioContext.createAnalyser();
-  analyserNode.fftSize = 2048;
-  inputPoint.connect(analyserNode);
+    analyserNode = audioContext.createAnalyser();
+    analyserNode.fftSize = 2048;
+    inputPoint.connect( analyserNode );
 
-  audioRecorder = new Recorder(inputPoint);
+    audioRecorder = new Recorder( inputPoint );
 
-  zeroGain = audioContext.createGain();
-  zeroGain.gain.value = 0.0;
-  inputPoint.connect(zeroGain);
-  zeroGain.connect(audioContext.destination);
-  updateAnalysers();
+    zeroGain = audioContext.createGain();
+    zeroGain.gain.value = 0.0;
+    inputPoint.connect( zeroGain );
+    zeroGain.connect( audioContext.destination );
+    updateAnalysers();
 }
 
 function initAudio() {
-  if (!navigator.getUserMedia)
-    navigator.getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-  if (!navigator.cancelAnimationFrame)
-    navigator.cancelAnimationFrame = navigator.webkitCancelAnimationFrame || navigator.mozCancelAnimationFrame;
-  if (!navigator.requestAnimationFrame)
-    navigator.requestAnimationFrame = navigator.webkitRequestAnimationFrame || navigator.mozRequestAnimationFrame;
+        if (!navigator.getUserMedia)
+            navigator.getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+        if (!navigator.cancelAnimationFrame)
+            navigator.cancelAnimationFrame = navigator.webkitCancelAnimationFrame || navigator.mozCancelAnimationFrame;
+        if (!navigator.requestAnimationFrame)
+            navigator.requestAnimationFrame = navigator.webkitRequestAnimationFrame || navigator.mozRequestAnimationFrame;
 
-  navigator.getUserMedia({
-    "audio": {
-      "mandatory": {
-        "googEchoCancellation": "false",
-        "googAutoGainControl": "false",
-        "googNoiseSuppression": "false",
-        "googHighpassFilter": "false"
-      },
-      "optional": []
-    },
-  }, gotStream, function(e) {
-    alert('Error getting audio');
-    console.log(e);
-  });
+    navigator.getUserMedia(
+        {
+            "audio": {
+                "mandatory": {
+                    "googEchoCancellation": "false",
+                    "googAutoGainControl": "false",
+                    "googNoiseSuppression": "false",
+                    "googHighpassFilter": "false"
+                },
+                "optional": []
+            },
+        }, gotStream, function(e) {
+            alert('Error getting audio');
+            console.log(e);
+        });
 }
 
-window.addEventListener('load', initAudio);
+window.addEventListener('load', initAudio );

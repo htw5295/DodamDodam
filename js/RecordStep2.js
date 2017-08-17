d442b9f420bdc48640c89a40321fbdb9b5b3d276
HwangTaeWook <xowktktjd@gmail.com>
2017-08-18 03:12:47 +0900

    test

--- js/RecordStep2.js
+++ js/RecordStep2.js
@@ -1,7 +1,36 @@
 var check = 1;
 
+var width = 368;
+var height = 169;
+
+window.onload = function() {
+  var widthRate = $('#bg').width() / 1920;
+  var heightRate = $('#bg').height() / 1861;
+
+  var icon = document.getElementsByClassName('Icon');
+
+  for (var i = 0; i < 2; i++) {
+    icon[i].width = iconSize * widthRate;
+    icon[i].height = iconSize * heightRate;
+  }
+}
+
+window.onresize = function() {
+  var widthRate = $('#bg').width() / 1920;
+  var heightRate = $('#bg').height() / 2258;
+
+  var icon = document.getElementsByClassName('Icon');
+
+  for (var i = 0; i < 2; i++) {
+    icon[i].width = width * widthRate;
+    icon[i].height = height * heightRate;
+  }
+}
+
 function onClickRecord() {
   if (check == 1) {
+    document.getElementById('start_button').style.display = "none";
+    document.getElementById('finish_button').style.display = "block";
     check = 2;
   } else {
     location.replace('../html/RecordStep3.html');
